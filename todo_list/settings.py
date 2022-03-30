17db78a3f2ea61c6667b2b795af66e7860704fa7
Aditya Bhattad <adityabhattad18@gmail.com>
2022-03-30 22:58:38 +0530

    removed typo

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoice.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'todo_list.urls'
